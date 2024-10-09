cd2ec29570222bfd84b23665d1bb3f209ad57a40
Ayman <ayman.asbai101@alu.ulpgc.es>
2024-10-09 23:42:41 +0100

    Main and Person classes created;

--- kata1/src/software/ulpgc/Main.java
+++ kata1/src/software/ulpgc/Main.java
@@ -1,4 +1,9 @@
 package software.ulpgc;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Person("Ayman", LocalDate.of(2004,9,24)));
+    }
 }
